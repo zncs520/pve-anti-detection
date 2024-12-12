d7049ae36a19b6dc3a80f87192fd4c7e2237c70f
李晓流 <2900629805@qq.com>
2024-12-12 22:08:11 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -2,6 +2,8 @@ git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
 apt install devscripts -y
 mk-build-deps --install -y
+make
+make clean
 cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
 cp ../smbios.h qemu/include/hw/firmware/smbios.h
 cp ../smbios.c qemu/hw/smbios/smbios.c
@@ -9,5 +11,6 @@ cd qemu
 chmod +x sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 bash sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 cd ..
-make clean
+apt install devscripts -y
+mk-build-deps --install -y
 make 
