93dbe032f9ea766b7beef40f75ee42400c0f82ed
李晓流 <2900629805@qq.com>
2024-12-12 22:24:32 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -1,8 +1,12 @@
+sudo apt-get insatll libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev
+librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
+ls
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
 apt install devscripts -y
 mk-build-deps --install -y
 make
+: <<'EOF'
 make clean
 cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
 cp ../smbios.h qemu/include/hw/firmware/smbios.h
@@ -14,3 +18,4 @@ cd ..
 apt install devscripts -y
 mk-build-deps --install -y
 make 
+EOF
