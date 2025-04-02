b8ff493f478e32b255349d494b0bad4aebbc0c66
李晓流 <2900629805@qq.com>
2025-04-02 21:42:33 +0800

    Update sedPatch-pve-qemu-kvm7-8-anti-dection.sh

--- sedPatch-pve-qemu-kvm7-8-anti-dection.sh
+++ sedPatch-pve-qemu-kvm7-8-anti-dection.sh
@@ -2,7 +2,7 @@
 #适用于给pve-qemu-kvm9里面的qemu打补丁使用，只支持9版本(不支持kvm7和kvm8，再高没有测试)，直接放本脚本在qemu目录下，在make包之前在qemu目录运行一次本脚本就是，运行后你可以继续使用git工具生成qemu具体版本的patch文件
 #参考开源项目 https://github.com/zhaodice/proxmox-ve-anti-detection 编写，处理重复劳作
 #作者 李晓流 & 大大帅666 20240824出品 https://space.bilibili.com/565938745
-brand="DELL"
+brand="DELL" #这里修改品牌，仅4个大写英文字母
 echo "开始sed工作"
 sed -i 's/QEMU v" QEMU_VERSION/'${brand}' v" QEMU_VERSION/g' block/vhdx.c
 sed -i 's/QEMU VVFAT", 10/'${brand}' VVFAT", 10/g' block/vvfat.c
