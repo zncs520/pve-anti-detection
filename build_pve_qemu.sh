af9d4150a339ebe0b2ff517da322256708119445
李晓流 <2900629805@qq.com>
2025-04-06 17:40:36 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -5,7 +5,7 @@ ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
-#git reset --hard 4f4fca78f7bb0ea6e4df6adda0b498e1918a5355
+git reset --hard 053f68a7aca508dc2dba9b8947b74a0704e51baf
 apt install devscripts -y
 mk-build-deps --install
 make
