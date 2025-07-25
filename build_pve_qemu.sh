3004521f070f467e92a7219d8e28eae40ac9d56d
李晓流 <2900629805@qq.com>
2025-07-25 19:52:50 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -16,9 +16,13 @@ chmod +x sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 bash sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 cp ../../smbios.h include/hw/firmware/smbios.h
 cp ../../smbios.c hw/smbios/smbios.c
+sed -i 's/!object_dynamic_cast/object_dynamic_cast/g' hw/vfio/igd.c
 git diff > qemu-autoGenPatch.patch
 cp qemu-autoGenPatch.patch ../
 cd ..
 apt install devscripts -y
 mk-build-deps --install
 make
+cd qemu/
+git checkout .
+cd ..
