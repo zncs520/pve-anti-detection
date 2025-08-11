f6dea7dd586088957cc4be1a50386dc4f7b193f1
李晓流 <2900629805@qq.com>
2025-08-12 02:51:27 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -1,6 +1,6 @@
 #以下两行docker里面运行需要sudo 如果pve里面运行请删除sudo
 sudo apt-get update
-sudo apt-get install -y libacl1-dev libaio-dev libgfapi0 libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
+sudo apt-get install -y libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
 ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
