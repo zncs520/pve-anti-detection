3981ebe0cd343cbda98e684225549590f739af42
李晓流 <2900629805@qq.com>
2025-08-13 02:50:44 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -8,9 +8,9 @@ cd pve-qemu
 git reset --hard 839b53bab89fddb7a7fb3a1d722e05df932cce4e
 apt install devscripts -y
 mk-build-deps --install
-git submodule update ---remote --init --recursive --force
 cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
 cd qemu
+git submodule update --init --recursive --force
 chmod +x sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 bash sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 cp ../../smbios.h include/hw/firmware/smbios.h
