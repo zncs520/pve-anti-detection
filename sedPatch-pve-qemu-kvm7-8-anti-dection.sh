2239c5eaab369334830d8345c8b2112e5c9ea1b1
李晓流 <2900629805@qq.com>
2025-08-14 23:31:01 +0800

    Update sedPatch-pve-qemu-kvm7-8-anti-dection.sh

--- sedPatch-pve-qemu-kvm7-8-anti-dection.sh
+++ sedPatch-pve-qemu-kvm7-8-anti-dection.sh
@@ -147,6 +147,7 @@ sed -i 's/#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x1af4/#define PCI_SUBVENDOR_
 sed -i 's/0x1af4/0x8086/g' hw/audio/hda-codec.c # QEMU_HDA_ID_VENDOR  0x1af4 =ich9-intel-hda
 #sed -i 's/rev = 3/rev = 4/g' hw/i386/acpi-build.c # Most VMs use an older-style FADT of length 244  bytes (revision  3), cutting off before the Sleep Control/Status registers and Hypervisor ID
 #sed -i 's/lat = 0xfff/lat = 0x1fff/g' hw/i386/acpi-build.c  # A value > 100 indicates the system does not support a C2/C3 state
+sed -i 's/build_waet(tables/\/\/build_waet(tables/g' hw/i386/acpi-build.c # build_waet(tables
 sed -i 's/0x0627/0x6666/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet
 sed -i 's/0x0627/0x6666/g' hw/usb/dev-hid.c # 0x0627=QEMU tablet
 echo "结束sed工作"
