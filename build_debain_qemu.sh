49ffc9e861edc9eb4c0232a71c1a1ec8ced59b74
李晓流 <2900629805@qq.com>
2025-10-14 20:10:51 +0800

    Update build_debain_qemu.sh

--- build_debain_qemu.sh
+++ build_debain_qemu.sh
@@ -8,8 +8,8 @@ df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
 #git reset --hard 839b53bab89fddb7a7fb3a1d722e05df932cce4e
-apt install devscripts -y
-mk-build-deps --install
+#apt install devscripts -y
+#mk-build-deps --install
 git submodule update --init --recursive
 cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
 cd qemu
