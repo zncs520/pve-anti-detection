d7fdab67e73d91cdc2193135120beb14feab159a
李晓流 <2900629805@qq.com>
2025-10-14 20:01:44 +0800

    Update build_debain_qemu.sh

--- build_debain_qemu.sh
+++ build_debain_qemu.sh
@@ -1,14 +1,15 @@
-#以下两行docker里面运行需要sudo，一共有4个sudo 如果pve里面运行请删除sudo
+#
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
+
+apt-get update
+apt-get install -y libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
 ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
 #git reset --hard 839b53bab89fddb7a7fb3a1d722e05df932cce4e
-sudo apt install devscripts -y
-sudo mk-build-deps --install
+apt install devscripts -y
+mk-build-deps --install
 git submodule update --init --recursive
 cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
 cd qemu
