2ae414a3c2f1be8471acb7ab09ba78a1b42f26af
李晓流 <2900629805@qq.com>
2025-10-16 13:03:06 +0800

    Update git reset commit hash in build script

--- build_debain_qemu.sh
+++ build_debain_qemu.sh
@@ -6,7 +6,7 @@ ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
-#git reset --hard 839b53bab89fddb7a7fb3a1d722e05df932cce4e
+git reset --hard 245689b9ae4120994de29b71595ea58abac06f3c
 #apt install devscripts -y
 #mk-build-deps --install
 git submodule update --init --recursive
