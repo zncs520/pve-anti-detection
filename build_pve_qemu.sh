22494a8f5575c9980432239dcd7f7887b6ce21d1
zncs520 <70083721@qq.com>
2025-11-23 22:08:00 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -14,8 +14,8 @@ cd qemu
 meson subprojects download
 chmod +x sedPatch-pve-qemu-kvm7-8-anti-dection.sh
 ##bash sedPatch-pve-qemu-kvm7-8-anti-dection.sh
-cp ../../smbios.h include/hw/firmware/smbios.h
-cp ../../smbios.c hw/smbios/smbios.c
+##cp ../../smbios.h include/hw/firmware/smbios.h
+##cp ../../smbios.c hw/smbios/smbios.c
 cp ../../bootsplash.jpg pc-bios/bootsplash.jpg # modify seabios bootsplash.jpg
 ##sed -i "s/vgabios.bin/vgabios.bin',\n\t'bootsplash.jpg/g" pc-bios/meson.build # modify seabios bootsplash.jpg
 ##sed -i 's/current_machine->boot_config.splash;/"\/usr\/share\/kvm\/bootsplash.jpg";/g' hw/nvram/fw_cfg.c # modify seabios bootsplash.jpg
